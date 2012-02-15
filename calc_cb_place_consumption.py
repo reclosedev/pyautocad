c467c3021cf86f607acde9b5cf256bf689c1fbf3
Roman <whatever@example.com>
2012-02-15 15:42:52 +0400

    calc_cb_place_consumption.py: added additional normalizing

--- calc_cb_place_consumption.py
+++ calc_cb_place_consumption.py
@@ -7,6 +7,7 @@ from pyautocad.utils import mtext_to_string, timing
 
 def main():
     acad = Autocad()
+    print u'Примерный подсчет занимаемого места'
     print '%-20s| %s' % (u'Имя щита', u'Общее число модулей')
     for layout in acad.iter_layouts():
         table = acad.find_one('table', layout.Block, lambda x: x.Columns == 5)
@@ -18,7 +19,11 @@ def main():
         while row < table.Rows:
             row += 1
             item_str = mtext_to_string(table.GetText(row, 2))
-            m = re.match(ur'.*(\d)-([xх]|но)\s+полюс.*', item_str)
+            item_str = item_str.replace(u'четырехполюсный', u'4-х')\
+                               .replace(u'трехполюсный', u'3-х')\
+                               .replace(u'двухполюсный', u'2-х')\
+                               .replace(u'однополюсный', u'1-но')
+            m = re.match(ur'.*(\d)-([xх]|но).*', item_str)
             if m:
                 n_modules = int(m.group(1))
                 quantity = int(mtext_to_string(table.GetText(row, 3)))
