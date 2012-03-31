3d7a767caae5959134f8a401c986c762eb7fed2e
Roman <reclosedev@gmail.com>
2012-03-31 18:14:46 +0400

    Fix exclude_scripts in cx_setup.py

--- cx_setup.py
+++ cx_setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#date: 14.02.12
 """
-Converts example scripts to Windows executables
+    cx_setup
+    ~~~~~~~~
+
+    Converts example scripts to Windows executables.
 """
 import glob
 import sys
@@ -11,16 +13,15 @@ from cx_Freeze import setup, Executable
 
 if not len(sys.argv[1:]):
     sys.argv.append('install_exe')
-    #sys.argv.append('build_exe')
 
 install_exe_options = {'install_dir': './Autocad tools'}
-build_exe_options = {'excludes': ['bz2', '_hashlib', 'unittest'
-                                   'tests'],} # TODO Experimental
+build_exe_options = {'excludes': ['bz2', '_hashlib', 'unittest', 'tests']}
                      
 
-exclude_scripts = ['cx_setup.py', '__init__.py']
+exclude_scripts = [r'examples\__init__.py', r'examples\dev_get_table_info.py']
 scripts_to_build = [name for name in glob.glob('examples/*.py') if
                     name not in exclude_scripts]
+
 
 setup(name="Autocad tools",
       version="0.1",
