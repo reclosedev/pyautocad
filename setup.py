6ca17b12ea7da8b3c5bd113dd05254591167f401
Roman Haritonov <reclosedev@gmail.com>
2012-06-02 12:51:53 +0400

    add optional setuptools to setup.py for "develop" command

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 
 setup(
-    name = "pyautocad",
-    packages = ["pyautocad", "pyautocad.contrib"],
-    version = "0.1.2",
-    description = "AutoCAD Automation for Python",
-    author = "Roman Haritonov",
-    author_email = "reclosedev@gmail.com",
-    url = "https://github.com/reclosedev/pyautocad",
+    name="pyautocad",
+    packages=["pyautocad", "pyautocad.contrib"],
+    version="0.1.2",
+    description="AutoCAD Automation for Python",
+    author="Roman Haritonov",
+    author_email="reclosedev@gmail.com",
+    url="https://github.com/reclosedev/pyautocad",
     install_requires=[
         'comtypes',
-    ],
-    keywords = ["autocad", "automation", "activex", "comtypes"],
-    license = "BSD License",
+        ],
+    keywords=["autocad", "automation", "activex", "comtypes"],
+    license="BSD License",
     include_package_data=True,
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
@@ -26,6 +29,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Scientific/Engineering",
-    ],
-    long_description = open('README.rst').read()
+        ],
+    long_description=open('README.rst').read()
 )
