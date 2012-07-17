9771824882e2d52ba15252d75588f64279bd2fb2
Roman Haritonov <reclosedev@gmail.com>
2012-07-17 16:06:21 +0400

    Rewrite get_names.py example using filter

--- examples/get_names.py
+++ examples/get_names.py
@@ -6,34 +6,39 @@ import sys
 from pyautocad import Autocad, APoint, utils
 
 
-def iter_drawings_names(acad, doc):
+acad = Autocad()
+
+
+def iter_drawings_names(doc):
     num_layouts = doc.Layouts.Count - 2
-    for layout_number, layout in enumerate(acad.iter_layouts(doc)):
+    layouts = doc.Layouts.filter(TabOrder__ne=0).order_by('TabOrder')
+    for layout_number, layout in enumerate(layouts):
         utils.dynamic_print('  Layout %02d/%02d' % (layout_number, num_layouts))
         # first we need to find our main stamp with name 'f4'
-        block = acad.find_one('blockreference', layout.Block, lambda x: 'f4' in x.EffectiveName)
+        block = layout.Block.filter(ObjectName='AcDbBlockReference',
+                                    EffectiveName='f4').first()
         if not block:
             continue
         block_pos = APoint(block.InsertionPoint)
         # approximate position of drawing name
         name_pos = block_pos + APoint(-90, 12)
-        for mt in acad.iter_objects("mtext", layout.Block):
-            if name_pos.distance_to(mt.InsertionPoint) < 5.0:
-                text = mt.TextString
-                yield text.replace(" \\P", " ").replace("\\P", " ")
-                break
+        mt = layout.Block.filter(ObjectName="AcDbMText",
+                                 InsertionPoint__dist_lt=(name_pos, 5.0)).first()
+        if mt is not None:
+            text = mt.TextString
+            yield text.replace(" \\P", " ").replace("\\P", " ")
     print
 
 def main():
     filename = sys.argv[1] if sys.argv[1:] else 'names.txt'
     output = codecs.open(filename, "w", encoding='utf-8')
-    acad = Autocad()
+
     for doc in acad.app.Documents:
         print doc.Name
         output.write(u'%s\n' % ('-' * 50))
         output.write(u"    %s\n" % doc.Name)
         output.write(u'%s\n' % ('-' * 50))
-        for drawing_name in iter_drawings_names(acad, doc):
+        for drawing_name in iter_drawings_names(doc):
             output.write(u'%s\n' % drawing_name)
 
 
